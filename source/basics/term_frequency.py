b36cd55434945e3906a66ad855addfd905d9a567
Aleksander Thornes Vestlund <aleksander.vestlund@hotmail.com>
2025-10-20 16:05:29 +0900

    feat: Update .gitignore

--- source/basics/term_frequency.py
+++ source/basics/term_frequency.py
@@ -8,16 +8,3 @@ def term_frequency(term: str, document: str) -> float:
     if (max_count := max(counter.values())) == 0:
         return 0.0
     return counter[term] / max_count
-
-
-def main() -> None:
-    print(
-        term_frequency(
-            "Aleksander",
-            "Aleksander var en kul mann. Han var kjempekul. Han het Aleksander. Han var en løk",
-        )
-    )
-
-
-if __name__ == "__main__":
-    main()
